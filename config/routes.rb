b0fcdea096f7ad0b20815cdfcbb3a8c06733dfe0
Naina Raisinghani <nainar@chromium.org>
2016-01-08 22:26:55 +1100

    final

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   resources :posts do
     resources :comments, only: [:create]
   end
+  get '/about_me' => 'pages#about_me'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
